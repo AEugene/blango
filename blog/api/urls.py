45e643de81d91e84595cce9c4b0f1cd3446ba0dd
Yevhen Andreiev <eugene.andreiev@gmail.com>
2022-07-09 20:32:37 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -20,6 +20,11 @@ urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
 
 from django.urls import path, include, re_path
